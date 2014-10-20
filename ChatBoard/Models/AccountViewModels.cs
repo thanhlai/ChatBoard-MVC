b3123b69e630cf36d19f646f70550f378f151e2e
thanhlai <thanhlai@users.noreply.github.com>
2014-10-20 13:25:09 -0700

    Add username

--- ChatBoard/Models/AccountViewModels.cs
+++ ChatBoard/Models/AccountViewModels.cs
@@ -70,6 +70,11 @@ namespace ChatBoard.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(25)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
---
+++ ChatBoard/Models/Public/PinnedAnswer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChatBoard.Models.Public
+{
+    public class PinnedAnswer
+    {
+        [ScaffoldColumn(false)]
+        public int PinnedAnswerID { get; set; }
+
+        public int BoardID { get; set; }
+
+        //Optional
+        public int UserID { get; set; }
+        
+        [Required]
+        public string UserName { get; set; }
+    }
+}
